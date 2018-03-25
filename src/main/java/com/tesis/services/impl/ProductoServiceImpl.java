49762c95eba72bea59baca8099bcbc4742dba351
Nahuel <nahuel_44_91@outlook.com>
2018-03-24 23:24:09 -0300

    fix services

--- src/main/java/com/tesis/services/impl/ProductoServiceImpl.java
+++ src/main/java/com/tesis/services/impl/ProductoServiceImpl.java
@@ -6,23 +6,25 @@ import com.tesis.services.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 /**
  * Created by Nahuel on 9/12/2017.
  */
 
-@RestController("ProductoServiceController")
+@RestController
 public class ProductoServiceImpl implements ProductoService {
 
     @Autowired
     ProductoBO productoBO;
 
-    @RequestMapping("/getProductoById")
+    @RequestMapping(value = "/getProductoById", method = GET)
     public Producto getProductoById(@RequestParam(value="id") Integer id) {
         return productoBO.getProductoById(id);
 
     }
 
-    @RequestMapping("/getProductoByCodigo")
+    @RequestMapping(value = "/getProductoByCodigo", method = GET)
     public Producto getProductoByCodigo(String codigo) {
         return productoBO.getProductoByCodigo(codigo);
     }
